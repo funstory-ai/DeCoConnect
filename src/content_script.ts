188944f448f69326833b66dfb171376e0effef98
Jiamin <jiamin@gmail.com>
2019-10-22 11:19:46 -0700

    rename a file with correct spelling; remove lines commented out earlier

--- src/content_script.ts
+++ src/content_script.ts
@@ -6,7 +6,7 @@ import {
   readExistingComments,
   getBook,
   getChapters,
-} from './libs/functuins';
+} from './libs/functions';
 import { getSelection } from './libs/getError';
 
 declare global {
@@ -61,42 +61,7 @@ function main(){
 }
 
 setTimeout(()=>{
-  // main();
-  getSelection();
+  main();
+  // getSelection();
 
-}, 3000)
-
-/*
-alert(currentUser);
-
-// First require it.
-var SideComments = require('side-comments');
-
-// Then, create a new SideComments instance, passing in the wrapper element and the optional the current user and any existing comments.
-sideComments = new SideComments('#commentable-area', currentUser, existingComments);
-
-// Listen to "commentPosted", and send a request to your backend to save the comment.
-// More about this event in the "docs" section.
-sideComments.on('commentPosted', function( comment ) {
-  $.ajax({
-    url: '/comments',
-    type: 'POST',
-    data: comment,
-    success: function( savedComment ) {
-      // Once the comment is saved, you can insert the comment into the comment stream with "insertComment(comment)".
-      sideComments.insertComment(comment);
-    }
-  });
-});
-
-// Listen to "commentDeleted" and send a request to your backend to delete the comment.
-// More about this event in the "docs" section.
-sideComments.on('commentDeleted', function( commentId ) {
-  $.ajax({
-    url: '/comments/' + commentId,
-    type: 'DELETE',
-    success: function( success ) {
-        // Do something.
-    }
-  });
-}); */
+}, 3000);
