d0deb7eab29a8dddb19a531ec1ef5ce61269f770
Zhou Linkai <zhoulinkai@funstory.ai>
2019-10-23 14:29:35 +0800

    merge getError showError into selections

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,23 +1,18 @@
 import './style/index.less';
 import { windowLoaded } from './libs/utils';
-// import * as Jquery from 'jquery';
-import { getSelection } from './libs/getError';
-import { showError } from './libs/showError';
+import { selection } from './libs/selections';
 import { sideComments } from './libs/sideComments'
+// import * as Jquery from 'jquery';
 
 
 async function init() {
   sideComments();
-
   // 由于url更新，整个js都会重新运行
   if (!window.janusWindowLoaded){
     window.janusWindowLoaded=true;
     await windowLoaded();
   }
-  
-  
-  getSelection();
-  showError();
+  selection();
 }
 
 init();
--- src/libs/selections.ts
+++ src/libs/selections.ts
@@ -1,9 +1,14 @@
 import * as database from './database';
 import { getBookInfo, getUser } from './utils';
+import * as CryptoJS from 'crypto-js';
 
 
 
-let selectionObj;
+export async function selection() {
+  selectionGetError();
+  showError();
+}
+
 
 export function optionBox() {
   let box = document.querySelector('#optionBoxDiv');
@@ -30,7 +35,7 @@ function hidebox() {
   
 }
 
-export async function getSelection() {
+export async function selectionGetError() {
   // console.log('window.optionBox',window.optionBox);
   // console.log('window loaded');
   const txtdom = window.document.querySelectorAll('pre')[0];
@@ -91,3 +96,35 @@ export async function getSelection() {
   }
   return true;
 }
+
+
+
+export async function showError() {
+  const { title, chapter } = getBookInfo();
+  const list = await database.getData(title, chapter, getUser());
+  if (!list) return;
+  const hashObj = {};
+  [...window.document.querySelectorAll('pre')[0].childNodes].forEach(node => {
+    hashObj[CryptoJS.SHA256(node.textContent).toString()] = node;
+  })
+  console.log(list);
+  console.log(hashObj);
+
+  Object.keys(list).forEach(key => {
+    const matchError = list[key];
+    if (hashObj[matchError.hash]) {
+      addColor(hashObj[matchError.hash], matchError)
+    }
+  })
+}
+
+export function addColor(matchNode: HTMLElement, matchError: ItextData) {
+  // let alltext = matchNode.firstChild.nodeValue;
+  let alltext = matchNode.innerText.trim();
+  matchNode.innerHTML = alltext
+    .split(matchError.text)
+    .join(`<span class="5janusError janusErrorType${matchError.errType}">${matchError.text}</span>`)
+  // matchNode.firstChild.nodeValue = alltext.split(matchError.text).join(`<span style="background:red;">${matchError.text}</span>`)
+}
+
+
